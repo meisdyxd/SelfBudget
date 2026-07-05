4bae1524f89514e4a32390f2f52b22b88fad7644
meisdy <kararturkar@gmail.com>
2026-07-05 21:16:12 +0300

    migrations

---
+++ src/SelfBudget/SelfBudget.API/Migrations/20260705163205_InitialMigration.cs
@@ -0,0 +1,315 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SelfBudget.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "account_types",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    name = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    description = table.Column<string>(type: "character varying(500)", maxLength: 500, nullable: true),
+                    created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    updated_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    created_by = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: true),
+                    updated_by = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_account_types_id", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "tags",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    name = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    updated_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    created_by = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: true),
+                    updated_by = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_tags_id", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "transaction_category_types",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    name = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_transaction_category_types_id", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    name = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    email = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    photo_id = table.Column<Guid>(type: "uuid", nullable: true),
+                    password_hash = table.Column<string>(type: "character varying(1000)", maxLength: 1000, nullable: false),
+                    birthdate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    updated_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    created_by = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: true),
+                    updated_by = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_users_id", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "transaction_categories",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    base_transaction_category_id = table.Column<Guid>(type: "uuid", nullable: true),
+                    transaction_category_type_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    name = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_transaction_categories_id", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_transaction_categories_transaction_categories_base_transact~",
+                        column: x => x.base_transaction_category_id,
+                        principalTable: "transaction_categories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_transaction_categories_transaction_category_types_transacti~",
+                        column: x => x.transaction_category_type_id,
+                        principalTable: "transaction_category_types",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "accounts",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    user_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    type_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    name = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    currency_code = table.Column<string>(type: "character varying(3)", maxLength: 3, nullable: false),
+                    balance = table.Column<decimal>(type: "numeric(18,2)", nullable: false),
+                    overdraft_limit = table.Column<decimal>(type: "numeric(18,2)", nullable: false),
+                    created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    updated_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    created_by = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: true),
+                    updated_by = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_accounts_id", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_accounts_account_types_type_id",
+                        column: x => x.type_id,
+                        principalTable: "account_types",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_accounts_users_user_id",
+                        column: x => x.user_id,
+                        principalTable: "users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "photos",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    user_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    uri = table.Column<string>(type: "character varying(2048)", maxLength: 2048, nullable: false),
+                    size = table.Column<int>(type: "integer", nullable: false),
+                    created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    updated_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    created_by = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: true),
+                    updated_by = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_photos_id", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_photos_users_user_id",
+                        column: x => x.user_id,
+                        principalTable: "users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "transactions",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    amount = table.Column<decimal>(type: "numeric(18,2)", nullable: false),
+                    from_account_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    to_account_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    transaction_category_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    note = table.Column<string>(type: "character varying(1000)", maxLength: 1000, nullable: true),
+                    created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    updated_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    created_by = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: true),
+                    updated_by = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_transactions_id", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_transactions_accounts_from_account_id",
+                        column: x => x.from_account_id,
+                        principalTable: "accounts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_transactions_accounts_to_account_id",
+                        column: x => x.to_account_id,
+                        principalTable: "accounts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_transactions_transaction_categories_transaction_category_id",
+                        column: x => x.transaction_category_id,
+                        principalTable: "transaction_categories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "transaction_tags",
+                columns: table => new
+                {
+                    TagId = table.Column<Guid>(type: "uuid", nullable: false),
+                    TransactionId = table.Column<Guid>(type: "uuid", nullable: false),
+                    assigned_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_transaction_tags", x => new { x.TagId, x.TransactionId });
+                    table.ForeignKey(
+                        name: "FK_transaction_tags_tags_TagId",
+                        column: x => x.TagId,
+                        principalTable: "tags",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_transaction_tags_transactions_TransactionId",
+                        column: x => x.TransactionId,
+                        principalTable: "transactions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_accounts_type_id",
+                table: "accounts",
+                column: "type_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_accounts_user_id",
+                table: "accounts",
+                column: "user_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_photos_user_id",
+                table: "photos",
+                column: "user_id",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_tags_name",
+                table: "tags",
+                column: "name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_transaction_categories_base_transaction_category_id",
+                table: "transaction_categories",
+                column: "base_transaction_category_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_transaction_categories_transaction_category_type_id",
+                table: "transaction_categories",
+                column: "transaction_category_type_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_transaction_tags_TransactionId",
+                table: "transaction_tags",
+                column: "TransactionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_transactions_from_account_id",
+                table: "transactions",
+                column: "from_account_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_transactions_to_account_id",
+                table: "transactions",
+                column: "to_account_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_transactions_transaction_category_id",
+                table: "transactions",
+                column: "transaction_category_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_users_email",
+                table: "users",
+                column: "email",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "photos");
+
+            migrationBuilder.DropTable(
+                name: "transaction_tags");
+
+            migrationBuilder.DropTable(
+                name: "tags");
+
+            migrationBuilder.DropTable(
+                name: "transactions");
+
+            migrationBuilder.DropTable(
+                name: "accounts");
+
+            migrationBuilder.DropTable(
+                name: "transaction_categories");
+
+            migrationBuilder.DropTable(
+                name: "account_types");
+
+            migrationBuilder.DropTable(
+                name: "users");
+
+            migrationBuilder.DropTable(
+                name: "transaction_category_types");
+        }
+    }
+}
