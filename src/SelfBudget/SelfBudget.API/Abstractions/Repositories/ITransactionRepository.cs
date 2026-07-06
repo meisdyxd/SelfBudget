92e1afa71dad9534ab46c29a8fe9fe31e2fa42ec
a-karlov <a-karlov@asoft21.ru>
2026-07-06 15:19:54 +0300

    feat: func get transaction by id

--- src/SelfBudget/SelfBudget.API/Abstractions/Repositories/ITransactionRepository.cs
+++ src/SelfBudget/SelfBudget.API/Abstractions/Repositories/ITransactionRepository.cs
@@ -1,8 +1,10 @@
-﻿using SelfBudget.API.Entities;
+﻿using SelfBudget.API.Dtos.Responses;
+using SelfBudget.API.Entities;
 
 namespace SelfBudget.API.Abstractions.Repositories;
 
 public interface ITransactionRepository
 {
     Task<Guid> CreateTransactionAsync(Transaction transaction, CancellationToken cancellationToken);
+    Task<TransferResponse> GetTransactionByIdAsync(Guid transactionId, CancellationToken cancellationToken);
 }
--- src/SelfBudget/SelfBudget.API/Repositories/TransactionRepository.cs
+++ src/SelfBudget/SelfBudget.API/Repositories/TransactionRepository.cs
@@ -1,5 +1,7 @@
-﻿using SelfBudget.API.Abstractions.Repositories;
+﻿using Microsoft.EntityFrameworkCore;
+using SelfBudget.API.Abstractions.Repositories;
 using SelfBudget.API.Database;
+using SelfBudget.API.Dtos.Responses;
 using SelfBudget.API.Entities;
 
 namespace SelfBudget.API.Repositories;
@@ -17,11 +19,34 @@ public class TransactionRepository : ITransactionRepository
         _logger = logger;
     }
 
-    public async Task<Guid> CreateTransactionAsync(Transaction transaction, CancellationToken cancellationToken)
+    public async Task<Guid> CreateTransactionAsync(
+        Transaction transaction,
+        CancellationToken cancellationToken)
     {
         await _dbContext.Transactions.AddAsync(transaction, cancellationToken);
 
         _logger.LogInformation("Создана новая транзакция с ID: '{TransactionId}'", transaction.Id);
         return transaction.Id;
+    }
+
+    public async Task<TransferResponse?> GetTransactionByIdAsync(
+        Guid transactionId,
+        CancellationToken cancellationToken)
+    {
+        var result = await _dbContext.Transactions
+            .Where(t => t.Id == transactionId)
+            .Select(t => new TransferResponse
+            {
+                Amount = t.Amount,
+                Id = t.Id,
+                CreatedAt = t.CreatedAt ?? DateTime.UtcNow,
+                FromAccountId = t.FromAccountId,
+                ToAccountId = t.ToAccountId,
+                Status = "success",
+                Note = t.Note
+            })
+            .FirstOrDefaultAsync(cancellationToken);
+
+        return result;
     }
 }
